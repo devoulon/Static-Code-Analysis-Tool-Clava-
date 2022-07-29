aabe8949227e23a06460748d4ee7b5ed04184aa6
thomas devoulon <92854540+devoulon@users.noreply.github.com>
2022-07-29 16:55:28 +0100

    Update AnalyserCWE120.js

--- clava/analysis_new/analysers/CWE/AnalyserCWE120.js
+++ clava/analysis_new/analysers/CWE/AnalyserCWE120.js
@@ -1,55 +1,62 @@
 
 laraImport("clava.analysis_new.analysers.CWE.Checker.CheckerString");
 laraImport("clava.analysis_new.analysers.CWE.Checker.CheckerArray");
+laraImport("clava.analysis_new.analysers.CWE.Report.MessageGenerator");
+laraImport("clava.analysis_new.analysers.CWE.Report.Weakness");
 
+class AnalyserCWE120 {
 
-class AnalyserCWE120   {
-	
-	 isBuffer=false
+	variable
 
-	 hasCopyFunction=false
+	isBuffer = false
 
-	 isSizeCheck=false
+	hasCopyFunction = false
 
-	  tabChecker=[this.isBuffer,this.hasCopyFunction,this.isSizeCheck]
+	isSizeCheck = false
 
-	 size =0
-	
-	 tabInt={}
-	
+	tabChecker = [this.isBuffer, this.hasCopyFunction, this.isSizeCheck]
+
+	size = 0
+
+	tabInt = {}
+
 	constructor() {
-		}
+	}
 
 
 
-	result(){
-		if(this.tabChecker[2]!==this.tabChecker[1]){
-			println( "weakness detected:120"+true)
-		}else{
-			println( "weakness detected:120"+false)
+	result() {
+		if (this.tabChecker[2] !== this.tabChecker[1]) {
+			println("weakness detected:120" + true)
+			MessageGenerator.append(new Weakness(this.variable, "weakness detected: CWE120"))
+		} else {
+			println("weakness detected:120" + false)
 		}
 	}
 
-	
-	analyse($node){ 
 
-		if($node===undefined){
+	analyse($node) {
+
+		if ($node === undefined) {
 			return false
 		}
+		if ($node.instanceOf("vardecl")) {
+			this.variable = $node
+		}
 		//  check if variable is a Buffer and get size
-		if(this.tabChecker[0] === false){
-			this.tabChecker[0]= CheckerString.isBufferInput($node)
-			if(this.tabChecker[0]===true){
-				this.size=CheckerArray.getSizeArray($node)
+		if (this.tabChecker[0] === false) {
+			this.tabChecker[0] = CheckerString.isBufferInput($node)
+			if (this.tabChecker[0] === true) {
+				this.size = CheckerArray.getSizeArray($node)
 			}
 			return this.tabChecker[0]
 		}
 		// check  a if condition which check size  
-		else if(this.tabChecker[1]===false){
-			if(CheckerString.isSensibleFunction($node)){
-				this.tabChecker[1]=true
-				this.tabChecker[2]=CheckerString.isVerifyingSize($node,this.size)
-			}			
+		else if (this.tabChecker[1] === false) {
+			if (CheckerString.isSensibleFunction($node)) {
+				this.tabChecker[1] = true
+				this.tabChecker[2] = CheckerString.isVerifyingSize($node, this.size)
+			}
 		}
 
 	}
